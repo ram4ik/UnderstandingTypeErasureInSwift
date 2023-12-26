fc0dfddd0d53d30dff0e77788e4721f0ec246658
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-26 09:23:45 +0200

    What is Type Erasure

--- UnderstandingTypeErasureInSwift/ContentView.swift
+++ UnderstandingTypeErasureInSwift/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+protocol Printable {
+    var data: Data { get }
+}
+
+struct Photo: Printable {
+    let image: Data
+    
+    var data: Data {
+        image
+    }
+}
+
+struct Letter: Printable {
+    let text: String
+    
+    var data: Data {
+        text.data(using: .utf8) ?? Data()
+    }
+}
+
+struct Postcard: Printable {
+    let text: String
+    
+    var data: Data {
+        text.data(using: .utf8) ?? Data()
+    }
+}
+
+struct Printer {
+//    func print(_ photo: Photo) {
+//        
+//    }
+//    func print(_ letter: Letter) {
+//        
+//    }
+    func print(_ printable: Printable) {
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
+        .onAppear() {
+            let printer = Printer()
+            printer.print(Photo(image: Data()))
+            printer.print(Letter(text: "My Letter"))
+            printer.print(Postcard(text: "My Postcard"))
+        }
     }
 }
 
