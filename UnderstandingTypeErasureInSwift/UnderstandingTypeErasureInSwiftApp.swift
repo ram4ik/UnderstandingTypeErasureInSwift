15a418a62b2bcebaaf8ca2028ee2fc7799902ecd
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-26 09:13:27 +0200

    Initial Commit

---
+++ UnderstandingTypeErasureInSwift/UnderstandingTypeErasureInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingTypeErasureInSwiftApp.swift
+//  UnderstandingTypeErasureInSwift
+//
+//  Created by test on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingTypeErasureInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
